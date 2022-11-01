99db82e09a230a398a390f54a3301d48f58466e7
chanwit-y <chanwit.yimneam@hotmail.com>
2022-11-01 19:25:22 +0700

    -

--- router/routers.go
+++ router/routers.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"go-demo-unit-test/internal/customer"
+	"go-demo-unit-test/internal/product"
 	"go-demo-unit-test/internal/user_profile"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,6 +13,7 @@ func PublicRoutes(app *fiber.App) {
 	routerV1 := app.Group("api/v1")
 
 	customer.Router(routerV1)
+	product.Router(routerV1)
 	user_profile.Router(routerV1)
 
 }
